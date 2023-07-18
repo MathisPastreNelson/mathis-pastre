ddc94cdd4a38569ffb86b763c0d6f46ecbf44b1f
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-07-18 19:47:41 +0200

    test for theme button

--- components/ThemeToggleButton.vue
+++ components/ThemeToggleButton.vue
@@ -1,38 +1,37 @@
 <template>
-  <button
-    aria-label="theme change"
-    @click="toggleTheme"
-    class="themeButton outline-none shadow-shadow1 flex justify-center items-center px-4 py-3 w-14 max-sm:w-12 max-sm:px-2 h-5vh rounded-lg font-black active:brightness-150 hover:brightness-150 ease-out duration-300">
-    <img :src="getIconSrc()" alt="theme-icon" class="icon-img" />
-  </button>
+  <div>
+    <button
+      aria-label="theme change"
+      class="themeButton outline-none shadow-shadow1 flex justify-center items-center w-14 px-2 py-3 max-sm:w-12 max-sm:px-2 mr-3 h-5vh rounded-lg font-black active:brightness-150 hover:brightness-150 ease-out duration-300"
+      @click="toggleTheme">
+      <img class="w-7" :src="currentIcon" alt="theme-icon" />
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      darkIcon: "~/assets/images/items/icons/Moon.svg",
-      lightIcon: "~/assets/images/items/icons/Sun.svg",
+      theme: "dark",
     };
   },
-  methods: {
-    getIconSrc() {
-      if (process.client) {
-        return localStorage.getItem("theme") === "light"
-          ? this.darkIcon
-          : this.lightIcon;
-      }
-      // Default icon when running SSR (server-side rendering)
-      return this.darkIcon;
+
+  computed: {
+    currentIcon() {
+      return this.theme === "light"
+        ? "~/assets/images/items/icons/Sun.svg"
+        : "~/assets/images/items/icons/Moon.svg";
     },
+  },
+
+  methods: {
     toggleTheme() {
-      // Avoid toggling theme when running SSR
-      if (process.client) {
-        const currentTheme = localStorage.getItem("theme");
-        const newTheme = currentTheme === "light" ? "dark" : "light";
-        localStorage.setItem("theme", newTheme);
-        this.$emit("toggle-theme", newTheme);
-      }
+      const newTheme = this.theme === "light" ? "dark" : "light";
+      this.theme = newTheme;
+      // You can perform any additional actions related to theme change here
+      // For example, save the theme in localStorage or emit an event to update the theme globally.
+      this.$emit("toggle-theme", newTheme);
     },
   },
 };
