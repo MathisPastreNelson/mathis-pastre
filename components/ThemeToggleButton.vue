180602a5330046bfa59684c86bf131455fbdceed
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-07-18 20:03:40 +0200

    toggle icon ok

--- components/ThemeToggleButton.vue
+++ components/ThemeToggleButton.vue
@@ -4,7 +4,7 @@
     @click="toggleTheme"
     class="themeButton outline-none shadow-shadow1 flex justify-center items-center px-4 py-3 w-14 max-sm:w-12 max-sm:px-2 h-5vh rounded-lg font-black active:brightness-150 hover:brightness-150 ease-out duration-300">
     <img
-      class="w-7"
+      class="w-4"
       v-if="theme === 'dark'"
       src="@/assets/images/items/Icons/Moon.svg"
       alt="French Flag" />
