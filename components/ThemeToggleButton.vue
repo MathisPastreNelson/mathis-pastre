212b9f7c825d5fec806120ac8a8527899aee0028
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-07-18 19:26:13 +0200

    improve project

--- components/ThemeToggleButton.vue
+++ components/ThemeToggleButton.vue
@@ -3,21 +3,36 @@
     aria-label="theme change"
     @click="toggleTheme"
     class="themeButton outline-none shadow-shadow1 flex justify-center items-center px-4 py-3 w-14 max-sm:w-12 max-sm:px-2 h-5vh rounded-lg font-black active:brightness-150 hover:brightness-150 ease-out duration-300">
-    <i class="text-1xl" :class="theme === 'dark' ? 'ii-moon' : 'ii-sun'"></i>
+    <img :src="getIconSrc()" alt="theme-icon" class="icon-img" />
   </button>
 </template>
 
 <script>
 export default {
-  props: {
-    theme: {
-      type: String,
-      required: true,
-    },
+  data() {
+    return {
+      darkIcon: "~/assets/images/items/icons/Moon.svg",
+      lightIcon: "~/assets/images/items/icons/Sun.svg",
+    };
   },
   methods: {
+    getIconSrc() {
+      if (process.client) {
+        return localStorage.getItem("theme") === "light"
+          ? this.darkIcon
+          : this.lightIcon;
+      }
+      // Default icon when running SSR (server-side rendering)
+      return this.darkIcon;
+    },
     toggleTheme() {
-      this.$emit("toggle-theme");
+      // Avoid toggling theme when running SSR
+      if (process.client) {
+        const currentTheme = localStorage.getItem("theme");
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        localStorage.setItem("theme", newTheme);
+        this.$emit("toggle-theme", newTheme);
+      }
     },
   },
 };
