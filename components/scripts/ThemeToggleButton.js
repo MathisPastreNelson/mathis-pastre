efa12e368a8a74bb146286b40642e16ce8b4d524
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2023-08-09 12:32:29 +0200

    default theme light

--- components/scripts/ThemeToggleButton.js
+++ components/scripts/ThemeToggleButton.js
@@ -3,7 +3,7 @@
 export default {
   getThemeFromLocalStorage() {
     if (typeof localStorage !== "undefined") {
-      return localStorage.getItem("theme") || "dark";
+      return localStorage.getItem("theme") || "light";
     }
     return "light";
   },
