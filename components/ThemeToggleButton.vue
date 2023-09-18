29fe03715dad136afd5916b8bf1f8185fdb81a04
MNPastre <shiray@hotmail.fr>
2023-09-18 11:49:38 +0200

    ok

--- components/ThemeToggleButton.vue
+++ components/ThemeToggleButton.vue
@@ -4,11 +4,13 @@
     aria-label="theme change"
     @click="toggleTheme"
     class="themeButton outline-none shadow-shadow1 flex justify-center items-center px-4 py-3 w-14 max-sm:w-12 max-sm:px-2 h-5vh rounded-lg font-black active:brightness-150 hover:brightness-150 ease-out duration-300">
+    <!-- Drapeau francais -->
     <img
       class="w-4"
       v-if="theme === 'dark'"
       src="~/assets/images/items/Icons/Moon.svg"
       alt="French Flag" />
+    <!-- Drapeau anglais -->
     <img
       class="w-7"
       v-else-if="theme === 'light'"
